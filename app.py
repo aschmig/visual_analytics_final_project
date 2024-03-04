ac88ed103faa26a2b3dde929b5fec6089c8747d4
aschmig <162066581+aschmig@users.noreply.github.com>
2024-03-04 15:05:08 -0500

    update

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import os
 print('Directory Path: ',os.getcwd())
 #print('Data Source Location: ',os.listdir())
 #print("Path: aschmig/visual_analytics_final_project/20240227-scimaps-export.csv")
-
-df = pd.read_csv('20240227-scimaps-export.csv')
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = '20240227-scimaps-export.csv'
+abs_file_path = os.path.join(script_dir, rel_path)
+df = pd.read_csv(abs_file_path)
 print(df)
