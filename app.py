706c3cc77a322837324673075063fd9a0a65261c
aschmig <162066581+aschmig@users.noreply.github.com>
2024-03-04 14:02:39 -0500

    File Locator

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import numpy as np
 import matplotlib as plt
 import os
 
-print(os.getcwd())
+print('Directory Path: ',os.getcwd())
+
+print('Data Source Location: ',os.path.dirname('20240227-scimaps-export.csv'))
 
 df = pd.read_csv('20240227-scimaps-export.csv')
 print(df)
