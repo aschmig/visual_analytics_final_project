96e6c1cd08c93c69a00b6c55c91b1757fa51450c
aschmig <162066581+aschmig@users.noreply.github.com>
2024-03-04 14:24:01 -0500

    test again

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ import matplotlib as plt
 import os
 
 print('Directory Path: ',os.getcwd())
-
-print('Data Source Location: ',os.listdir())
-
-print("Path: aschmig/visual_analytics_final_project/20240227-scimaps-export.csv")
+#print('Data Source Location: ',os.listdir())
+#print("Path: aschmig/visual_analytics_final_project/20240227-scimaps-export.csv")
 
 df = pd.read_csv('/20240227-scimaps-export.csv')
 print(df)
